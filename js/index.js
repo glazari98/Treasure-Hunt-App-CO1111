b22e971b26cc2f5c567f1d641710072ec0229463
Chystiakova Daria <123006049+breadoorr@users.noreply.github.com>
2024-02-24 13:43:45 +0000

    landing page (unfinished)

---
+++ js/index.js
@@ -0,0 +1,24 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            console.log(entry);
+            if(entry.isIntersecting) {
+                entry.target.classList.add('show');
+            }
+            else {
+                entry.target.classList.remove('show');
+            }
+        });
+    });
+    
+    const hidden = document.querySelectorAll('.hidden');
+    hidden.forEach((el) => observer.observe(el));
+
+    window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-529VLQPE0H');
+});
