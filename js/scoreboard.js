3797be9680bf179a26c4c73d23a8b31fe4e45fb8
Georgios Lazari <128077479+glazari98@users.noreply.github.com>
2024-03-13 20:29:42 +0200

    fixed some errors for redirecting the user to scoreboard.html when he finishes the quiz, using the correct folder to find the javascript file in scoreboard.html and the correct filder to find the chest image. Also added the cookies function for the fetch to work in scoreboard.js

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -1,3 +1,17 @@
+var cookies1 = document.cookie;
+function getCookieValue(cookieName){
+    let array = cookieName.split("; ");
+    for(let i=0; i < array.length; i++){
+        let cookie = array[i];
+        let [name,value] =  cookie.split("=");
+        if (name == "session"){
+            return value;
+        }
+    }
+
+}
+let sessionID = getCookieValue(cookies1);
+
 async function getScoreAndDisplay() {
     let scoreElement = document.getElementById('score');
     await fetch("https://codecyprus.org/th/api/score?session=" + sessionID)
