4afae4bbc7c08825364cc033313344a5ddfc58a3
Vasilii <154226913+VTelnov@users.noreply.github.com>
2024-03-14 09:34:37 +0000

    sessionStorage version

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -1,24 +1,7 @@
-var cookies1 = document.cookie;
-function getCookieValue(cookieName){
-    let array = cookieName.split("; ");
-    for(let i=0; i < array.length; i++){
-        let cookie = array[i];
-        let [name,value] =  cookie.split("=");
-        if (name == "session"){
-            return value;
-        }
-    }
+var playerScore = sessionStorage.getItem("playerScore");
 
-}
-let sessionID = getCookieValue(cookies1);
-
-async function getScoreAndDisplay() {
-    let scoreElement = document.getElementById('score');
-    await fetch("https://codecyprus.org/th/api/score?session=" + sessionID)
-        .then(response => response.json())
-        .then(jsonObject => {
-            scoreElement.innerText = "Score: " + jsonObject.score;
-        });
+var scoreElement = document.getElementById("score");
+if (playerScore !== null) {
+    scoreElement.textContent = "Your Score: " + playerScore;
 }
 
-getScoreAndDisplay();
