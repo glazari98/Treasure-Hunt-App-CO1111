f11f2a78c43dd07a6b94b534bc485d827f060d20
Georgios Lazari <128077479+glazari98@users.noreply.github.com>
2024-03-23 19:28:16 +0200

    Added functionality to show the ranking of the user's score when in the leaderboard. Modified some styling in app.html, scoreboard.html leaderboard.html. the user can now post after he finished the quiz. Modified the fucntions for /start and from treasure hunt list to catch some errors if the user has completed it before.

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ fetch("https://codecyprus.org/th/api/list").then(response => response.json())
 .then(jsonObject => {
     console.log(jsonObject.treasureHunts); 
     //TODO - Success, do something with the data.
-    const ul = document.getElementById("challenges");
+    const container = document.getElementById("challenges");
     // var array = jsonObject[0].name;
     // console.log(array);
     let cD = new Date();  
@@ -11,18 +11,23 @@ fetch("https://codecyprus.org/th/api/list").then(response => response.json())
         
     // console.log(element);
     let valid = (element.startsOn <= cD) ?  true : false;
-    const li = document.createElement('li');
-    li.innerHTML = element.name;
-    if(!valid) li.classList.add('invalid');
-    else { li.classList.add('valid'); }
-    li.classList.add('alertbutton');
-    ul.appendChild(li);
+    const button = document.createElement('button');
+    button.innerHTML = element.name;
+    if(!valid) {
+        button.classList.add('invalid');
+        button.disabled = true;
+    }
+    else {
+        button.classList.add('valid');
+    }
+    button.classList.add('alertbutton');
+   container.appendChild(button);
    
 
-      li.addEventListener('click', function() {
-        let huntName = li.textContent;
+      button.addEventListener('click', function() {
+        let huntName = button.textContent;
         console.log(huntName);
-        if(valid) alert("Starting treasure hunt " + huntName);
+        if(valid) alert("Starting treasure hunt: " + huntName);
         window.location.href = "start.html?treasure-hunt-id=" + element.uuid;
         localStorage.setItem("timeOfTH", element.maxDuration);
       });
